999b1df1cc001a7b3b670b81f68ffe119e55a0f0
doanvinhphu <doanvinhphu321@gmail.com>
2025-07-29 04:56:03 +0700

    validate export request ID only if it is not null, refactor application.yml to use environment variable for JPA ddl-auto setting

--- src/main/java/capstonesu25/warehouse/service/ImportOrderService.java
+++ src/main/java/capstonesu25/warehouse/service/ImportOrderService.java
@@ -72,9 +72,12 @@ public class ImportOrderService {
         ImportRequest importRequest = importRequestRepository.findById(request.getImportRequestId())
                 .orElseThrow(() -> new NoSuchElementException(
                         "ImportRequest not found with ID: " + request.getImportRequestId()));
-        if(!importRequest.getExportRequestId().equals(request.getExportRequestId())) {
-            throw new IllegalStateException("Cannot create import order: Import request and export request IDs not match");
+        if (importRequest.getExportRequestId() != null) {
+            if(!importRequest.getExportRequestId().equals(request.getExportRequestId())) {
+                throw new IllegalStateException("Cannot create import order: Import request and export request IDs not match");
+            }
         }
+
         boolean canBeContinued = false;
         if(importRequest.getType().equals(ImportType.ORDER)) {
             for (ImportRequestDetail detail : importRequest.getDetails()) {
